eef48c44a34f0ddcb03043f9337f90c0874cd2f3
ardiansyahfauzi <arfa.fauzi@gmail.com>
2018-04-24 15:10:06 +0900

    Add files via upload

---
+++ sub_bathymetry.f90
@@ -0,0 +1,19 @@
+  !! ----------------------------------------------------------------------- !!
+  !>
+  !! Bathymetry depth hh(:,:) ( in m-unit ) must be filled here. 
+  !<
+  block
+
+    integer :: i, j
+    !! --
+   
+    ! modified by Fauzi
+    open(21,file="../bath_in_new.dat", status='unknown')
+    
+    do i=1,nx
+      read(21,*)(hh(i,j), j=1,ny)
+    enddo
+    close(21)
+    !-------------------
+
+  end block
---
+++ sub_initheight.f90
@@ -0,0 +1,19 @@
+  !! ----------------------------------------------------------------------- !!
+  !>
+  !! Tsunami initial height.
+  !! Tsunami height eta(:,:) ( in m-unit ) must be defined here.
+  !<
+  !! --
+  block
+   
+    integer :: i, j
+    
+    open(22,file="../init_inv_45min.dat", status='old')
+    
+    do i=1,nx
+      read(22,*)(eta(i,j), j=1,ny)
+    enddo
+    
+    close(22)
+
+  end block
